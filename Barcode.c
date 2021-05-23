85babfa66530b0ee57f1690648a7c94a2d81dae9
Tanya Todorova <todorova9898@abv.bg>
2021-05-23 15:35:50 +0300

    edit round and decode functions
    Co-authored-by: Anastas Avdzhiev <aavdzhiev@users.noreply.github.com>

--- Barcode.c
+++ Barcode.c
@@ -18,15 +18,9 @@ t_Barcode* initializeBarcode(void) {
     return new_barcode;
 }
 
-
-int roundNum(double number)
-{
-    return (number >= 0 && number <= 0.5) ? 0 : 1;
-}
-
 void roundData(t_Barcode *barcode) {
     for (int i = 0; i < barcode->N; i++) {
-        barcode->data[i] = roundNum(barcode->data[i]);
+        barcode->data[i] = ((barcode->data[i] >= 0 && barcode->data[i] <= 0.5) ? 0 : 1);
     }
 } 
 
@@ -66,56 +60,55 @@ void processData(t_Barcode *b, char *code){
     
 }
 
-void decodeBarcode(char *barcode){
-    //char str[6];
+/*void decodeBarcode(char *barcode){
     char * token = strtok(barcode, " ");
-   // loop through the string to extract all other tokens
-    while( token != NULL ) {
-      //printf( "%s\n", token ); //printing each token
-      token = strtok(NULL, " ");
-      decodeSymbol(token);
+    while( token != NULL ) {     
+        token = strtok(NULL, " ");
+        decodeSymbol(token);
    }
-    
+}*/
 
+
+
+void decodeBarcode(char *barcode, char *result){
+    char * token = strtok(barcode, " "); //
+    while( token != NULL  ) {     
+        token = strtok(NULL, " ");
+        strcat(result, decodeSymbol(token));
+    }
+    //return result;
 }
 
-char decodeSymbol(char *str){
-    char symbol;
-    if (!(strcmp(str,"00001"))){
-        symbol = '0';
-    } else if (!(strcmp(str,"10001"))){
-        symbol = '1';
-    } else if (!(strcmp(str,"01001"))){
-        symbol = '2';
-    } else if (!(strcmp(str,"11001"))){
-        symbol = '3';
-    } else if (!(strcmp(str,"00101"))){
-        symbol = '4';
-    } else if (!(strcmp(str,"10100"))){
-        symbol = '5';
-    } else if (!(strcmp(str,"01100"))){
-        symbol = '6';
-    } else if (!(strcmp(str,"00011"))){
-        symbol = '7';
-    } else if (!(strcmp(str,"10010"))){
-        symbol = '8';
-    } else if (!(strcmp(str,"10000"))){
-        symbol = '9';
-    } else if (!(strcmp(str,"00100"))){
-        symbol = '-';
-    } else if (!(strcmp(str,"00110"))){
-        return 0;
+const char * decodeSymbol(char *str){
+    char *symbol;
+    if (!(strcmp(str, "00001"))) {
+        strcpy(symbol, "0");
+    } else if (!(strcmp(str, "10001"))) {
+        strcpy(symbol, "1");
+     } else if (!(strcmp(str, "01001"))) {
+        strcpy(symbol, "2");
+     } else if (!(strcmp(str, "11001"))) {
+        strcpy(symbol, "3");
+     } else if (!(strcmp(str, "00101"))) {
+        strcpy(symbol, "4");
+     } else if (!(strcmp(str, "10100"))) { 
+        strcpy(symbol, "5");
+     } else if (!(strcmp(str, "01100"))) {
+        strcpy(symbol, "6");
+    } else if (!(strcmp(str, "00011"))) {
+        strcpy(symbol, "7");
+    } else if (!(strcmp(str, "10010"))){
+        strcpy(symbol, "8");
+    } else if (!(strcmp(str, "10000"))){
+        strcpy(symbol, "9");
+    } else if (!(strcmp(str, "00100"))){
+        strcpy(symbol, "-");
+    } else if (!(strcmp(str, "00110"))){
+        strcpy(symbol, "S");
     } else {
-        printf("wrong barcode");
-        return 0;
+        printf("Barcode truncated. Move the scanner to the left.");
+        return 0; /*exit?*/
     }
-    printf("%c",symbol);
     return symbol;
 
 }
-/*
-символ 0 1 2 3 4 5
-кодиране 00001 10001 01001 11001 00101 10100
-символ 6 7 8 9 - / 10                   start/stop
-кодиране 01100 00011 10010 10000 00100 00110
-*/
--- main.c
+++ main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include "Barcode.h"
+#define STR_SIZE 50
+
 //#include "helper_func.c"
 
 int main() {
     t_Barcode *barcode = initializeBarcode();
     roundData(barcode);
-    //printBarcode(barcode);
+    // printBarcode(barcode);
 
     char *byteCode;
+    char *result;
+    
     byteCode[0] = '\0';
+    result[0] = '\0';
     processData(barcode, byteCode);
     //processData(barcode);
     // printf("The barcode is:\n");
     //printf("\n%s",byteCode);
     //printf("\n%c\n",decodeSymbol("00100"));
-    decodeBarcode(byteCode);
-
+    decodeBarcode(byteCode, result);
+    printf("str: %s", result);
+    
+    //decodeBarcode(byteCode,result);
+    //char *test = decodeSymbol("10001");
+    //printf("%s",test);
 
 
 
